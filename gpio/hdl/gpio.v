75cb28040b74a97e01a3d2bc620df6c54d209c49
Thomas Chou <thomas@wytron.com.tw>
2009-04-23 13:52:27 +0800

    gpio: remove open drain support
    Changed to tcl.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- gpio/hdl/gpio.v
+++ gpio/hdl/gpio.v
@@ -1,18 +1,20 @@
 //                              -*- Mode: Verilog -*-
 // Filename        : gpio.v
 // Description     : a bitwise gpio
-// Created On      : Mon Jan 22 09:38:50 2007
-// Status          : Unknown, Use with caution!
+// Author          : Thomas Chou
 
 // this is a bitwise gpio to be used with i2c,spi,sdio,1 wire etc.
-// it is not like Altera's pio, each access is bit by bit.
-// currently up to 16 pins can be addressed.
+// it is unlike Altera's pio, each access is bit by bit.
+// it is designed to work with generic gpio interface of Linux
+// so that it will run faster and use less LEs
+// you may turn on FAST_OUTPUT_REGISTER for the port pins to reduce LEs usage further
+// interrupt is not supported
 // port pin[i] can be addressed with base+(i*4)
-// the port can operate at two output modes, 
-// writedata[1] : 0=open-drain (default), 1=totem-pole
+// writedata[1] : output enable
 // writedata[0] : output data
 // readdata[0] : input data from pin
-
+// paramter WIDTH for number of io pins, ADDR the address width
+// WIDTH <= 2^ADDR
 
 module gpio (
              /*AUTOARG*/
@@ -23,40 +25,39 @@ module gpio (
    // Inputs
    address, clk, reset_n, write_n, writedata
    );
-   
 
-
-   parameter WIDTH = 2;
+   parameter GPIO_WIDTH = 8;
+   parameter GPIO_ADDR = 3;
    
-   inout [  WIDTH-1: 0] bidir_port;
-   output [  0: 0] 	readdata;
-   input [  3: 0] 	address;
+   inout [  GPIO_WIDTH-1: 0] bidir_port;
+   output [  1: 0] 	readdata;
+   input [  GPIO_ADDR-1: 0] 	address;
    input 		clk;
    input 		reset_n;
    input 		write_n;
    input [  1: 0] 	writedata;
 
-   reg [  WIDTH-1: 0] 	bidir_port;
-   reg [  0: 0] 	readdata;
-   reg [  WIDTH-1: 0] 	data_mode;
-   reg [  WIDTH-1: 0] 	data_outz;
-   reg [  WIDTH-1: 0] 	data_mode_v;
-   reg [  WIDTH-1: 0] 	data_outz_v;
+   reg [  GPIO_WIDTH-1: 0] 	bidir_port;
+   reg [  1: 0] 	readdata;
+   reg [  GPIO_WIDTH-1: 0] 	data_mode;
+   reg [  GPIO_WIDTH-1: 0] 	data_outz;
+   reg [  GPIO_WIDTH-1: 0] 	data_mode_v;
+   reg [  GPIO_WIDTH-1: 0] 	data_outz_v;
    integer N;
    
    always @(data_mode or data_outz)
-     for (N = 0; N <= (WIDTH-1) ; N = N+1)
-       bidir_port[N] = data_mode[N]? (data_outz[N]? 1'b0:1'b1) : (data_outz[N]? 1'b0:1'bz);
+     for (N = 0; N <= (GPIO_WIDTH-1) ; N = N+1)
+       bidir_port[N] = data_mode[N]? ~data_outz[N] : 1'bz;
 
    always @(/*AS*/address or bidir_port)
-     readdata[0] = bidir_port[address];
+     readdata = { 1'b0, bidir_port[address] };
    
    always @(/*AS*/address or data_outz or write_n or writedata)
-     for (N = 0; N <= (WIDTH-1) ; N = N+1)
+     for (N = 0; N <= (GPIO_WIDTH-1) ; N = N+1)
        data_outz_v[N] = (~write_n & (address == N)) ? ~writedata[0] : data_outz[N];
 
    always @(/*AS*/address or data_mode or write_n or writedata)
-     for (N = 0; N <= (WIDTH-1) ; N = N+1)
+     for (N = 0; N <= (GPIO_WIDTH-1) ; N = N+1)
        data_mode_v[N] = (~write_n & (address == N)) ? writedata[1] : data_mode[N];
 
    always @(posedge clk or negedge reset_n)
